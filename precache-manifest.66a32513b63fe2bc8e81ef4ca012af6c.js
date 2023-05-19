a4b388cb0b776f63cc3bfda64156d332cb75c9c5
Nikita Gromov <nikita.gromoff@gmail.com>
2023-05-19 12:51:40 +0300

    Updates

---
+++ precache-manifest.66a32513b63fe2bc8e81ef4ca012af6c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "03f60543c229bffcf3e8",
+    "url": "/static/css/main.ac0c2058.chunk.css"
+  },
+  {
+    "revision": "03f60543c229bffcf3e8",
+    "url": "/static/js/main.03f60543.chunk.js"
+  },
+  {
+    "revision": "2321672d6ebd36dc350f",
+    "url": "/static/js/1.2321672d.chunk.js"
+  },
+  {
+    "revision": "75f8d036225c654ae309",
+    "url": "/static/js/2.75f8d036.chunk.js"
+  },
+  {
+    "revision": "d2887abce2670504c86d",
+    "url": "/static/js/runtime~main.d2887abc.js"
+  },
+  {
+    "revision": "cdbabb8113cdf888c636dd65af7f5d56",
+    "url": "/static/media/upload_prof_pic.cdbabb81.svg"
+  },
+  {
+    "revision": "8dc79ec1b9687e4a936ee2a04f740915",
+    "url": "/static/media/upload_prof_pic_upload.8dc79ec1.svg"
+  },
+  {
+    "revision": "a944f5182c8717e1a22f2d6ffec6d8b9",
+    "url": "/static/media/authorized_Sam.a944f518.jpg"
+  },
+  {
+    "revision": "a1f0baffb99754b59060ad57c9bf4759",
+    "url": "/index.html"
+  }
+];
